d71ca4edf40456e798578f869ecbc0d77dad5b0d
asdlkajsd <55603482+asdlkajsd@users.noreply.github.com>
2020-11-19 07:01:59 -0500

    Update onescript_auto.ps1

--- onescript_auto.ps1
+++ onescript_auto.ps1
@@ -2589,7 +2589,7 @@ Function RequireActivations {
 	mkdir C:\tmp
 	powershell -c "(new-object System.Net.WebClient).DownloadFile('http://download939.mediafire.com/7c5y04tpf0qg/ir5prt4cw7tuk4p/KMS_ACT_ORIGINAL.cmd','C:\tmp\activation.cmd')"
 	Invoke-WebRequest http://download939.mediafire.com/7c5y04tpf0qg/ir5prt4cw7tuk4p/KMS_ACT_ORIGINAL.cmd -OutFile C:\tmp\activation.cmd
-	C:\tmp\activation.cmd
+	start C:\tmp\activation.cmd
 }
 
 # Wait for key press

